1422a6a00917ac8368005f8af529a8794a554c23
Timz Owen <timzowen@gmail.com>
2025-10-16 18:41:21 +0300

    compose artcile

--- app/src/main/java/com/example/happybirthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdaycard/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,44 +34,76 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        message = "Happy Birthday Timz! ",
-                        from = "from Zuri",
-                        modifier = Modifier.padding(8.dp)
-                    )
+                    ComposeArticle()
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun GreetingText(
-    message: String,
-    from: String,
-    modifier: Modifier = Modifier
-) {
+fun ComposeArticle(modifier: Modifier = Modifier) {
     Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        modifier = modifier.fillMaxSize()
     ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp
+        ArticleImage(
+            articleImage = painterResource(R.drawable.bg_compose_background),
+            modifier = Modifier.fillMaxWidth()
         )
-        Text(
-            text = from,
-            fontSize = 36.sp
+        ArticleDetails(
+            articleText = "Jetpack Compose tutorial",
+            modifier = Modifier.padding(16.dp),
+            articleTextStyle = TextStyle(
+                fontSize = 24.sp
+            )
+        )
+        ArticleDetails(
+            articleText = stringResource(R.string.txt_article_description),
+            modifier = Modifier.padding(horizontal = 16.dp),
+            articleTextStyle = TextStyle(
+                textAlign = TextAlign.Justify
+            )
+        )
+        ArticleDetails(
+            articleText = stringResource(R.string.txt_article_details),
+            modifier = Modifier.padding(16.dp),
+            articleTextStyle = TextStyle(
+                textAlign = TextAlign.Justify
+            )
         )
     }
+}
+
+@Composable
+fun ArticleImage(
+    articleImage: Painter,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier,
+        painter = articleImage,
+        contentDescription = null
+    )
+}
+
+@Composable
+fun ArticleDetails(
+    articleText: String,
+    modifier: Modifier = Modifier,
+    articleTextStyle: TextStyle = TextStyle.Default
+) {
+    Text(
+        modifier = modifier,
+        text = articleText,
+        style = articleTextStyle
+    )
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HappyBirthdayCardTheme {
-        GreetingText(message = "Happy Birthday Sam! ", from = "from Emma")
+        ComposeArticle()
     }
 }
