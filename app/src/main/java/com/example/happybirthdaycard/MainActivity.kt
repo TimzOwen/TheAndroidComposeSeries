0a460a315d913700666bc77d3a094bcaa7299673
Timz Owen <timzowen@gmail.com>
2025-10-16 21:29:21 +0300

    Business card compose basics

--- app/src/main/java/com/example/happybirthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdaycard/MainActivity.kt
@@ -4,19 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.happybirthdaycard.ui.theme.GreenColor
 import com.example.happybirthdaycard.ui.theme.HappyBirthdayCardTheme
+import com.example.happybirthdaycard.ui.theme.ImageDarkColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,44 +47,95 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        message = "Happy Birthday Timz! ",
-                        from = "from Zuri",
-                        modifier = Modifier.padding(8.dp)
-                    )
+                    BusinessCardAp()
                 }
             }
         }
     }
 }
 
+@Composable
+fun BusinessCardAp() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier.background(ImageDarkColor)
+            ) {
+                Image(
+                    modifier = Modifier.size(150.dp),
+                    painter = painterResource(R.drawable.android_logo),
+                    contentDescription = null
+                )
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = "Timz Owen",
+                fontSize = 28.sp
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "Android Developer Extraordinaire",
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Bold,
+                color = GreenColor
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Column(
+            modifier = Modifier.padding(bottom = 32.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            ContactInfoComponent(
+                contactIcon = Icons.Default.Call,
+                contactText = "+11 (123) 444 555 666"
+            )
+            ContactInfoComponent(
+                contactIcon = Icons.Default.Share,
+                contactText = "@AndroidDevTeam"
+            )
+            ContactInfoComponent(
+                contactIcon = Icons.Default.Email,
+                contactText = "timz@androidgoogle.com"
+            )
+        }
+    }
+}
 
 @Composable
-fun GreetingText(
-    message: String,
-    from: String,
-    modifier: Modifier = Modifier
+fun ContactInfoComponent(
+    contactIcon: ImageVector,
+    contactText: String
 ) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
+    Row(
+        horizontalArrangement = Arrangement.Start,
     ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp
+        Icon(
+            imageVector = contactIcon,
+            contentDescription = null,
+            tint = GreenColor,
         )
+        Spacer(modifier = Modifier.width(10.dp))
         Text(
-            text = from,
-            fontSize = 36.sp
+            text = contactText
         )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HappyBirthdayCardTheme {
-        GreetingText(message = "Happy Birthday Sam! ", from = "from Emma")
+        BusinessCardAp()
     }
 }
