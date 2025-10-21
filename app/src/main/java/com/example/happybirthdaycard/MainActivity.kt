25369e8d1d91001d8792d959f8211d19a638236d
Timz Owen <timzowen@gmail.com>
2025-10-21 22:44:04 +0300

    Art space app

--- app/src/main/java/com/example/happybirthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdaycard/MainActivity.kt
@@ -4,15 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,10 +51,63 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        message = "Happy Birthday Timz! ",
-                        from = "from Zuri",
-                        modifier = Modifier.padding(8.dp)
+                    ArtSpaceMain()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ArtWithTitle(
+    modifier: Modifier = Modifier,
+    artPicture: Int,
+    artTitle: String,
+    artistName: String,
+    yearOfRelease: String = ""
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Image(
+            painter = painterResource(artPicture),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(20.dp)
+                .height(300.dp)
+                .width(300.dp)
+                .border(border = BorderStroke(2.dp, Color.Gray), shape = RectangleShape)
+                .shadow(elevation = 4.dp)
+                .padding(10.dp)
+        )
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 45.dp, vertical = 90.dp)
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = artTitle,
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Start,
+                    lineHeight = 30.sp
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ){
+                    Text(
+                        text = artistName,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = modifier.padding(top = 8.dp)
+                    )
+                    Text(
+                        text = "($yearOfRelease)",
+                        fontSize = 16.sp,
+                        modifier = modifier.padding(top = 8.dp)
                     )
                 }
             }
@@ -41,31 +117,103 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun GreetingText(
-    message: String,
-    from: String,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
+fun ArtSpaceMain() {
+    var result by remember { mutableStateOf(1) }
+
+    when (result) {
+        1 -> {
+            ArtWithTitle(
+                artPicture = R.drawable.art_1,
+                artTitle = "The Ninja Boy",
+                artistName = "Timz Owen",
+                yearOfRelease = "2021"
+            )
+        }
+
+        2 -> {
+            ArtWithTitle(
+                artPicture = R.drawable.art_2,
+                artTitle = "The Elephant",
+                artistName = "Wild Life",
+                yearOfRelease = "2019"
+            )
+        }
+
+        3 -> {
+            ArtWithTitle(
+                artPicture = R.drawable.art_3,
+                artTitle = "The Flower Girl",
+                artistName = "mrs flower",
+                yearOfRelease = "2020"
+            )
+        }
+
+        4 -> {
+            ArtWithTitle(
+                artPicture = R.drawable.art_4,
+                artTitle = "The Pot ",
+                artistName = "The Potter",
+                yearOfRelease = "2018"
+            )
+        }
+
+        5 -> {
+            ArtWithTitle(
+                artPicture = R.drawable.art_5,
+                artTitle = "The Artist",
+                artistName = "Kelvin arts",
+                yearOfRelease = "2022"
+            )
+        }
+
+        else -> {
+            ArtWithTitle(
+                artPicture = R.drawable.art_5,
+                artTitle = "The Artist",
+                artistName = "Kelvin arts",
+                yearOfRelease = "2029"
+            )
+        }
+    }
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 700.dp)
     ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp
-        )
+        Button(
+            onClick = { result-- },
+            shape = RoundedCornerShape(50.dp),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 20.dp
+            ),
+            border = BorderStroke(2.dp, Color.Gray)
+        ) {
+            Text("Back")
+        }
+
+        Button(
+            onClick = { result++ },
+            shape = RoundedCornerShape(50.dp),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 20.dp
+            ),
+            border = BorderStroke(2.dp, Color.Gray)
+        ) {
+            Text(stringResource(R.string.next))
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    HappyBirthdayCardTheme {
-        GreetingText(message = "Happy Birthday Sam! ", from = "from Emma")
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        HappyBirthdayCardTheme {
+            ArtSpaceMain()
+        }
     }
 }
