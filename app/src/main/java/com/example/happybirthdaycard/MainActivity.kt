1927f77963a7120f546198e3ea54d116a05be49c
Timz Owen <timzowen@gmail.com>
2025-10-31 06:03:58 +0300

    woof app part one

--- app/src/main/java/com/example/happybirthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdaycard/MainActivity.kt
@@ -4,68 +4,157 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.happybirthdaycard.ui.theme.HappyBirthdayCardTheme
+import com.example.happybirthdaycard.dog.Dog
+import com.example.happybirthdaycard.dog.dogs
+import com.example.happybirthdaycard.ui.theme.WoofTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HappyBirthdayCardTheme {
+            WoofTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        message = "Happy Birthday Timz! ",
-                        from = "from Zuri",
-                        modifier = Modifier.padding(8.dp)
-                    )
+                    WoofApp()
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun GreetingText(
-    message: String,
-    from: String,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp
-        )
+fun WoofApp() {
+    Scaffold(
+        topBar = { WoofTopBar() }
+    ) { it ->
+        LazyColumn(
+            contentPadding = it,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(dogs) { dog ->
+                DogCardComponent(
+                    dog = dog
+                )
+            }
+        }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WoofTopBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_woof_logo),
+                    contentDescription = stringResource(R.string.app_name),
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun DogCardComponent(dog: Dog) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 8.dp)
+            .height(64.dp),
+        shape = RoundedCornerShape(topEnd = 16.dp, bottomStart = 16.dp)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Box(
+                modifier = Modifier
+                    .size(64.dp)
+            ) {
+                Image(
+                    painter = painterResource(dog.dogImageResourceId),
+                    contentDescription = stringResource(dog.dogNameResourceId),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small))
+                        .clip(MaterialTheme.shapes.small)
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(dog.dogNameResourceId),
+                    style = MaterialTheme.typography.displayMedium
+                )
+                Text(
+                    text = stringResource(R.string.years_old, dog.dogAgeResourceId),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    HappyBirthdayCardTheme {
-        GreetingText(message = "Happy Birthday Sam! ", from = "from Emma")
+    WoofTheme {
+        WoofApp()
+    }
+}
+
+@Preview
+@Composable
+fun WoofDarkThemePreview() {
+    WoofTheme(darkTheme = true) {
+        WoofApp()
     }
 }
